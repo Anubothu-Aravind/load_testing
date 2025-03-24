04faf893e074d83ceb381bc70949a40d5d67ee50
ANUBOTHU ARAVIND <2200080137@kluniversity.in>
2025-03-24 23:12:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-locust -f locustfile.py --host=https://klef.websico.in --users=5000 --spawn-rate=50 --headless --logfile=locust.log
+locust -f script.py --host=https://klef.websico.in --users=5000 --spawn-rate=50 --headless --logfile=locust.log
 
